afb53efc1237b27307d84ec8e7abfac8479e5073
hari saputra <hari.saputra.dev@gmail.com>
2024-09-30 09:59:34 +0700

    [update] with interface

--- main.go
+++ main.go
@@ -2,84 +2,120 @@ package main
 
 import "fmt"
 
+type Celcius struct {
+	suhu float64
+}
+type Farenheit struct {
+	suhu float64
+}
+type Kelvin struct {
+	suhu float64
+}
+
+func (c Celcius) toCelcius() float64 {
+	return c.suhu
+}
+
+func (c Celcius) toFarenheit() float64 {
+	return (9.0 / 5.0 * c.suhu) + 32
+}
+
+func (c Celcius) toKelvin() float64 {
+	return c.suhu + 273.15
+}
+
+func (f Farenheit) toFarenheit() float64 {
+	return f.suhu
+}
+
+func (f Farenheit) toCelcius() float64 {
+	return (f.suhu - 32) * (5.0 / 9.0)
+}
+
+func (f Farenheit) toKelvin() float64 {
+	return (f.suhu + 459.67) * (5.0 / 9.0)
+}
+
+func (k Kelvin) toKelvin() float64 {
+	return k.suhu
+}
+
+func (k Kelvin) toCelcius() float64 {
+	return k.suhu - 273.15
+}
+
+func (k Kelvin) toFarenheit() float64 {
+	return (k.suhu * 9.0 / 5.0) - 459.67
+}
+
+type tempCalc interface {
+	toCelcius() float64
+	toFarenheit() float64
+	toKelvin() float64
+}
+
 func main() {
-	fmt.Println("Please fill in the Number from the list bellow: ")
+	fmt.Println("Please fill in the Number from the first temp. list bellow: ")
 
-	fmt.Println("1. Celcius To Farenheit")
-	fmt.Println("2. Celcius To Kelvin")
-	fmt.Println("3. Farenheit To Celcius")
-	fmt.Println("4. Farenheit To Kelvin")
-	fmt.Println("5. Kelvin To Celcius")
-	fmt.Println("6. Kelvin To Farenheit")
+	fmt.Println("1. Celcius")
+	fmt.Println("2. Farenheit")
+	fmt.Println("3. Kelvin ")
 
-	println("list number: ")
+	println("first list number: ")
 
-	var list int
-	fmt.Scanf("%d", &list)
+	var first_list int
+	fmt.Scanf("%d", &first_list)
 
-	for list < 1 || list > 6 {
+	for first_list < 1 || first_list > 6 {
 		fmt.Println("Your choice not available, please fill with right choice: ")
-		fmt.Scanf("%d", &list)
+		fmt.Scanf("%d", &first_list)
 	}
 
-	var inputSuhu float64
-	fmt.Println("fill temp.: ")
-	fmt.Scanf("%f", &inputSuhu)
+	fmt.Println("Please fill in the Number from the Last temp. list bellow: ")
 
-	var outputSuhu float64
-	switch list {
+	fmt.Println("1. Celcius")
+	fmt.Println("2. Farenheit")
+	fmt.Println("3. Kelvin ")
+
+	println("last list number: ")
+
+	var last_list int
+	fmt.Scanf("%d", &last_list)
+
+	for last_list < 1 || last_list > 6 {
+		fmt.Println("Your choice not available, please fill with right choice: ")
+		fmt.Scanf("%d", &last_list)
+	}
+
+	var temp float64
+	fmt.Println("fill temp: ")
+	fmt.Scanf("%f", &temp)
+
+	var interfaceTemp tempCalc
+	switch first_list {
 	case 1:
-		outputSuhu = CelciusToFahrenheit(inputSuhu)
+		interfaceTemp = Celcius{temp}
 	case 2:
-		outputSuhu = CelciusToKelvin(inputSuhu)
+		interfaceTemp = Farenheit{temp}
 	case 3:
-		outputSuhu = FarenheitToCelcius(inputSuhu)
-	case 4:
-		outputSuhu = FarenheitToKelvin(inputSuhu)
-	case 5:
-		outputSuhu = KelvinToCelcius(inputSuhu)
-	case 6:
-		outputSuhu = KelvinToFarenheit(inputSuhu)
+		interfaceTemp = Kelvin{temp}
 	default:
 		fmt.Println("your choice not valid")
 		return
 	}
 
-	fmt.Printf("Temp. is: %.2f\n", outputSuhu)
-}
+	var finalTemp float64
+	switch last_list {
+	case 1:
+		finalTemp = interfaceTemp.toCelcius()
+	case 2:
+		finalTemp = interfaceTemp.toFarenheit()
+	case 3:
+		finalTemp = interfaceTemp.toKelvin()
+	default:
+		fmt.Println("your choice not valid")
+		return
+	}
 
-func CelciusToFahrenheit(celcius float64) float64 {
-	farenheit := (9.0 / 5.0 * celcius) + 32
-
-	return farenheit
-}
-
-func CelciusToKelvin(celcius float64) float64 {
-	kelvin := celcius + 273.15
-
-	return kelvin
-}
-
-func FarenheitToCelcius(farenheit float64) float64 {
-	celcius := (farenheit - 32) * (5.0 / 9.0)
-
-	return celcius
-}
-
-func FarenheitToKelvin(farenheit float64) float64 {
-	kelvin := (farenheit + 459.67) * (5.0 / 9.0)
-
-	return kelvin
-}
-
-func KelvinToCelcius(kelvin float64) float64 {
-	celcius := kelvin - 273.15
-
-	return celcius
-}
-
-func KelvinToFarenheit(kelvin float64) float64 {
-	farenheit := (kelvin * 9.0 / 5.0) - 459.67
-
-	return farenheit
+	fmt.Printf("Temp. is: %.2f\n", finalTemp)
 }
